331f840c2fac282908fae442a2908fc7d8cfb794
Joe <schirmer.pc@gmail.com>
2022-04-22 14:52:22 -0400

    trying to pub to ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   
